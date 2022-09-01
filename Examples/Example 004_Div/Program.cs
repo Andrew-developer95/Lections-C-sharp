e63440c2727f80bc9337376b533eaaa523dae3ac
Лефтер Андрей <andrew299347@gmail.com>
2022-09-01 20:43:39 +0200

    Деление

---
+++ Examples/Example 004_Div/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 10; // int - позволяет работать с целыми числами
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+double numberC = 12; // int - позволяет работать с вещественными числами
+double numberD = 5;
+Console.WriteLine(numberC / numberD);
